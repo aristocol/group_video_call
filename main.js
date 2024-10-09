5e309ab3d0b9ac6fc4e9ecaa519bab7918ce7839
aristocol <sandarbhsinghal0802@gmail.com>
2024-10-09 23:01:24 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,116 @@
+const APP_ID = "600e893face94204992ec44f86d93944";
+const TOKEN =
+  "007eJxTYLDxaGKXTfsstW6jj8eD6y6HGsIeTK14zOWUIbf271e1ecIKDGYGBqkWlsZpicmpliZGBiaWlkapySYmaRZmKZbGliYmy7fPSWsIZGSoW3eDhZEBAkF8FobcxMw8BgYAJMgffw==";
+const CHANNEL = "main";
+
+const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+
+let localTracks = [];
+let remoteUsers = {};
+
+let joinAndDisplayLocalStream = async () => {
+  client.on("user-published", handleUserJoined);
+
+  client.on("user-left", handleUserLeft);
+
+  let UID = await client.join(APP_ID, CHANNEL, TOKEN, null);
+
+  localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
+
+  let player = `<div class="video-container" id="user-container-${UID}">
+                        <div class="video-player" id="user-${UID}"></div>
+                  </div>`;
+  document
+    .getElementById("video-streams")
+    .insertAdjacentHTML("beforeend", player);
+
+  localTracks[1].play(`user-${UID}`);
+
+  await client.publish([localTracks[0], localTracks[1]]); //0 for audio 1 for video.
+};
+
+let joinStream = async () => {
+  await joinAndDisplayLocalStream();
+  document.getElementById("join-btn").style.display = "none";
+  document.getElementById("btn").style.display = "none";
+  document.getElementById("stream-controls").style.display = "flex";
+};
+
+let handleUserJoined = async (user, mediaType) => {
+  remoteUsers[user.uid] = user;
+  await client.subscribe(user, mediaType);
+
+  if (mediaType === "video") {
+    let player = document.getElementById(`user-container-${user.uid}`);
+    if (player != null) {
+      player.remove();
+    }
+
+    player = `<div class="video-container" id="user-container-${user.uid}">
+                        <div class="video-player" id="user-${user.uid}"></div> 
+                 </div>`;
+    document
+      .getElementById("video-streams")
+      .insertAdjacentHTML("beforeend", player);
+
+    user.videoTrack.play(`user-${user.uid}`);
+  }
+
+  if (mediaType === "audio") {
+    user.audioTrack.play();
+  }
+};
+
+let handleUserLeft = async (user) => {
+  delete remoteUsers[user.uid];
+  document.getElementById(`user-container-${user.uid}`).remove();
+};
+
+let leaveAndRemoveLocalStream = async () => {
+  for (let i = 0; localTracks.length > i; i++) {
+    localTracks[i].stop(); //stop the audio, video
+    localTracks[i].close(); //close and release the space
+  }
+
+  await client.leave();
+  document.getElementById("join-btn").style.display = "block";
+  document.getElementById("btn").style.display = "block";
+  document.getElementById("stream-controls").style.display = "none";
+  document.getElementById("video-streams").innerHTML = "";
+};
+
+let goback = async (e) => {
+  window.location.href = "https://s7rwwx-3000.csb.app/";
+};
+
+let toggleMic = async (e) => {
+  if (localTracks[0].muted) {
+    await localTracks[0].setMuted(false);
+    e.target.innerText = "Mic on";
+    e.target.style.backgroundColor = "cadetblue";
+  } else {
+    await localTracks[0].setMuted(true);
+    e.target.innerText = "Mic off";
+    e.target.style.backgroundColor = "#EE4B2B";
+  }
+};
+
+let toggleCamera = async (e) => {
+  if (localTracks[1].muted) {
+    await localTracks[1].setMuted(false);
+    e.target.innerText = "Camera on";
+    e.target.style.backgroundColor = "cadetblue";
+  } else {
+    await localTracks[1].setMuted(true);
+    e.target.innerText = "Camera off";
+    e.target.style.backgroundColor = "#EE4B2B";
+  }
+};
+
+document.getElementById("join-btn").addEventListener("click", joinStream);
+document.getElementById("btn").addEventListener("click", goback);
+document
+  .getElementById("leave-btn")
+  .addEventListener("click", leaveAndRemoveLocalStream);
+document.getElementById("mic-btn").addEventListener("click", toggleMic);
+document.getElementById("camera-btn").addEventListener("click", toggleCamera);
